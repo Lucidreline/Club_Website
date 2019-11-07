458c8c1b82dc84a1c86f535e806c750d22079e3e
Manny <ma52castaneda@gmail.com>
2019-11-07 13:55:30 -0800

    Added links to the home page photos

--- routes/mainPages.js
+++ routes/mainPages.js
@@ -10,36 +10,38 @@ router.get("/", function(req, res){
     res.redirect("/home");
 })
 
-router.get("/home", async function(req, res){
+router.get("/home",function(req, res){
     var boardMembers, members, announcements;
-    //await means we can move on from the code untl it is completely finished
-    await Member.find({boardMember:true}, function(err, foundBoardMembers){
+    //means we can move on from the code untl it is completely finished
+    Member.find({boardMember:true}, function(err, foundBoardMembers){
         if(err){
-            console.log("Could not find members for /home")
+            console.log("Could not find board members for /home... : " + err.message);
         }else{
             boardMembers = foundBoardMembers;
+            Member.find({}).sort({"_id": -1}).exec(function(err, foundMembers) {
+                if(err){
+                    console.log("Could not find members for /home... : " + err.message);
+                }else{
+                    members = foundMembers;
+                    Announcement.find({}).sort({"_id": -1}).exec(function(err, foundAnnouncements) {
+                        if(err){
+                            console.log("Could not find announcements for /home... : " + err.message);
+                        }else{
+                            announcements = foundAnnouncements;
+                            res.render("mainPages/home",{bMembers: boardMembers, allMembers: members, announcements: announcements});
+                        }
+                    }) 
+                }
+            }) 
         }
     }) 
-    //Blog.find({}).sort({"_id": -1}).exec(function(err, blogs) {....});
-    //await Member.find({}, function(err, foundMembers){
-    //await Member.find({}, function(err, foundMembers){
-    await Member.find({}).sort({"_id": -1}).exec(function(err, foundMembers) {
-        if(err){
-            console.log("Could not find members for /home")
-        }else{
-            members = foundMembers;
-        }
-    }) 
+   
+    
 
-    await Announcement.find({}, function(err, foundAnnouncements){
-        if(err){
-            console.log("Could not find members for /home")
-        }else{
-            announcements = foundAnnouncements;
-        }
-    }) 
+    
 
-    res.render("mainPages/home",{bMembers: boardMembers, allMembers: members, announcements: announcements});
+
+    
    
 })
 
