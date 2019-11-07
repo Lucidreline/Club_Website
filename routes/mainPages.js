5768e1f43db3fb5ff3ccd1dd2fd9d1e17ba99551
Manny <ma52castaneda@gmail.com>
2019-11-07 13:15:26 -0800

    The 2 newest members are displayed on the home page

--- routes/mainPages.js
+++ routes/mainPages.js
@@ -3,21 +3,44 @@ var router = express.Router();
 
 //This is requiring the schema for a member
 var Member = require("../models/member");
+var Announcement = require("../models/announcement");
+
 
 router.get("/", function(req, res){
     res.redirect("/home");
 })
 
-router.get("/home", function(req, res){
+router.get("/home", async function(req, res){
     var boardMembers, members, announcements;
-    Member.find({boardMember:true}, function(err, foundMembers){
+    //await means we can move on from the code untl it is completely finished
+    await Member.find({boardMember:true}, function(err, foundBoardMembers){
         if(err){
             console.log("Could not find members for /home")
         }else{
-            res.render("mainPages/home",{boardMembers: foundMembers});
+            boardMembers = foundBoardMembers;
         }
-    })
-    
+    }) 
+    //Blog.find({}).sort({"_id": -1}).exec(function(err, blogs) {....});
+    //await Member.find({}, function(err, foundMembers){
+    //await Member.find({}, function(err, foundMembers){
+    await Member.find({}).sort({"_id": -1}).exec(function(err, foundMembers) {
+        if(err){
+            console.log("Could not find members for /home")
+        }else{
+            members = foundMembers;
+        }
+    }) 
+
+    await Announcement.find({}, function(err, foundAnnouncements){
+        if(err){
+            console.log("Could not find members for /home")
+        }else{
+            announcements = foundAnnouncements;
+        }
+    }) 
+
+    res.render("mainPages/home",{bMembers: boardMembers, allMembers: members, announcements: announcements});
+   
 })
 
 module.exports = router;
